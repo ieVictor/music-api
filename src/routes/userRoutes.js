20cce657f7aa7236b9de5dae45ae9b2338ba770b
Victor Gabriel <victor_tiss@outlook.com>
2024-09-05 20:15:26 -0300

    feat(user): add user documentation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,262 @@
-const { Router } = require("express");
-const UserController = require("../controllers/userController");
+const { Router } = require('express');
+const UserController = require('../controllers/userController');
+const { authAdmin, authMiddleware } = require('../middlewares/auth');
 const router = Router();
 
-router.get('/', UserController.getUsers);
-router.put('/:userId', UserController.updateUser);
-router.post('/', UserController.createUser);
-router.delete('/:userId', UserController.deleteUser);
+/**
+ * @swagger
+ * tags:
+ *  name: User
+ *  description: Routes for user manipulation
+ */
 
-module.exports = router
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Get all users
+ *     description: Retrieves a list of all users. Requires admin privileges.
+ *     tags:
+ *       - User
+ *     operationId: get_all_users
+ *     security:
+ *       - jwt: []
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/UserList"
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Unauthorized"
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Forbidden"
+ *       500:
+ *         description: InternalServerError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/InternalServerError"
+ */
+router.get('/', authMiddleware, authAdmin, UserController.getUsers);
+
+/**
+ * @swagger
+ * /user/{userId}:
+ *   put:
+ *     summary: Update user
+ *     description: Updates information for a specific user by their ID.
+ *     tags:
+ *       - User
+ *     operationId: update_user
+ *     security:
+ *       - jwt: []
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         description: ID of the user to be updated
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UserUpdate"
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/User"
+ *       400:
+ *         description: BadRequest
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/BadRequest"
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Unauthorized"
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Forbidden"
+ *       404:
+ *         description: NotFound
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/NotFound"
+ *       500:
+ *         description: InternalServerError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/InternalServerError"
+ */
+router.put('/:userId', authMiddleware, UserController.updateUser);
+
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Create a new user
+ *     description: Creates a new user with the provided data.
+ *     tags:
+ *       - User
+ *     operationId: create_user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UserInsert"
+ *     responses:
+ *       201:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/User"
+ *       400:
+ *         description: BadRequest
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/BadRequest"
+ *       500:
+ *         description: InternalServerError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/InternalServerError"
+ */
+router.post('/', UserController.createUser);
+
+/**
+ * @swagger
+ * /users/admin:
+ *   post:
+ *     summary: Create a new admin user
+ *     description: Creates a new admin user. Requires admin privileges.
+ *     tags:
+ *       - User
+ *     operationId: create_admin
+ *     security:
+ *       - jwt: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UserInsert"
+ *     responses:
+ *       201:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/User"
+ *       400:
+ *         description: BadRequest
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/BadRequest"
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Unauthorized"
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Forbidden"
+ *       500:
+ *         description: InternalServerError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/InternalServerError"
+ */
+router.post('/admin', authMiddleware, authAdmin, UserController.createAdmin);
+
+/**
+ * @swagger
+ * /users/{userId}:
+ *   delete:
+ *     summary: Delete user by ID
+ *     description: Deletes a user by their ID.
+ *     tags:
+ *       - User
+ *     operationId: delete_user
+ *     security:
+ *       - jwt: []
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         description: ID of the user to be deleted
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/DeleteSuccess"
+ *       400:
+ *         description: BadRequest
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/BadRequest"
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Unauthorized"
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Forbidden"
+ *       404:
+ *         description: NotFound
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/NotFound"
+ *       500:
+ *         description: InternalServerError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/InternalServerError"
+ */
+router.delete('/:userId', authMiddleware, UserController.deleteUser);
+
+module.exports = router;
